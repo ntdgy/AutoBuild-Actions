74fa5ba684b175331823f31b030f9ebe5ad41d84
ntdgy <ntdgy2001@gmail.com>
2021-04-27 21:55:01 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -4,7 +4,7 @@
 # AutoBuild DiyScript
 
 Diy_Core() {
-	Author=Hyy2001
+	Author=ntdgy2001
 	Default_Device=x86_64
 	Short_Firmware_Date=true
 	Default_IP_Address=192.168.1.1
@@ -24,6 +24,7 @@ Firmware-Diy() {
 	d-team_newifi-d2)
 		Replace_File CustomFiles/mac80211.sh package/kernel/mac80211/files/lib/wifi
 		Replace_File CustomFiles/system_d-team_newifi-d2 package/base-files/files/etc/config system
+		 AddPackage git https://github.com/immortalwrt/luci-app-unblockneteasemusic.git luci-app-unblockneteasemusic https://github.com/immortalwrt/luci-app-unblockneteasemusic.git master
 		# Replace_File CustomFiles/Patches/102-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.4
 	;;
 	esac
